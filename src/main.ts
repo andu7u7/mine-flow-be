411b69776845eb10978bb8e849958dead1d3679f
Andy Jefersson Rojas Munares <ar0330yt@gmail.com>
2025-07-19 18:23:33 -0500

    feat: add cors origin address *

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: 'Content-Type, Accept, Authorization',
