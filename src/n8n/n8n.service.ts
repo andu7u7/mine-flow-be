055dfde927405a4af05a1eedcb4ba5084995bf6a
Andy Jefersson Rojas Munares <ar0330yt@gmail.com>
2025-07-18 19:00:14 -0500

    feat: agregamos n8n url a .env

--- src/n8n/n8n.service.ts
+++ src/n8n/n8n.service.ts
@@ -2,16 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class N8nService {
+  private readonly n8nUrl = process.env.URL_N8N as string;
+
   async testing(): Promise<any> {
-    const req = await fetch(
-      'http://n8n.krico.dev/webhook-test/3bf73c6e-c6fa-485f-bfd8-3e9e542524ea',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+    const req = await fetch(this.n8nUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
       },
-    );
+    });
 
     if (req.status !== 200) {
       throw new Error(`Error al llamar a n8n: ${req.status} ${req.statusText}`);
